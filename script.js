fc996305f2878dfceac70e4d3fc93e7a591ea3dc
marcelofms1 <43277551+marcelofms1@users.noreply.github.com>
2022-02-05 12:20:11 -0500

    created interactive hamburger menu with JS

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-console.log("Hello World!")
-
-alert("Welcome to my Portfolio Website!")
+function menuToggle (){
+    var x = document.getElementById ("myNavtoggle");
+    if (x.className === 'navtoggle'){
+      x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle'
+    }
+}
